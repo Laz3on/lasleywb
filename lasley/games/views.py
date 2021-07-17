dcb0cfc840cf493a182971277bbed0fd7faca2a1
Chris Lasley <laz3on@gmail.com>
2021-07-16 21:38:55 -0400

    updated games/views.py and games.html

--- lasley/games/views.py
+++ lasley/games/views.py
@@ -8,22 +8,12 @@ url = "https://www.freetogame.com/api/games"
 headers = {'Accept': 'application/json'}
 # Create your views here.
 
+
 def index(request):
     gameList = {}
     resp = requests.get(url, headers)
     games = json.loads(resp.text)
-    print(games)
-    # games = serializers.serialize('json', games)
-    # for game in games:
-    #     gameImg = game['thumbnail']
-    #     gameTitle = game['title']
-    #     gameDesc = game['short_description']
-    #     gameGenre = game['genre']
-    #     gamePlatform = game['platform']
-    #     gameReleaseDate = game['release_date']
-    #     gameURL = game['game_url']
-    #     gameDeveloper = game['developer']
-        # gameList = gameList.append(gameImg,gameTitle,gameDesc,gameGenre,gamePlatform,gameReleaseDate,gameURL,gameDeveloper)
+    games = games[0]
 
-    return render(request, 'games.html', context=games)
+    return render(request, 'games.html', games)
     # return HttpResponse(games)
