4c865b96b84b93e638af85ad6e5c947a0098be05
Chris Lasley <laz3on@gmail.com>
2021-10-23 15:56:39 -0400

    chand game app to be showed in cards

--- lasley/games/views.py
+++ lasley/games/views.py
@@ -2,11 +2,12 @@ from typing import List
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core import serializers
+from django.core.paginator import Paginator
 import requests
 import json
 
 url = "https://www.freetogame.com/api/games"
-headers = {'Accept': 'application/json'}
+headers = {"Accept": "application/json"}
 # Create your views here.
 
 
@@ -16,6 +17,9 @@ def index(request):
     games = json.loads(resp.text)
     # games = games[10]
     # print(games)
+    paginator = Paginator(games, 25)
 
-    return render(request, 'games.html', {'games':list(games)})
+    # page_number = request.Get.get('page')
+    # page_obj = paginator.get_page(page_number)
+    return render(request, "games.html", {"games": games})
     # return HttpResponse(games)
